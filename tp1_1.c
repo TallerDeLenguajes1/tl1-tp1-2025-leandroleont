def0330a74bf3e8feca01ce5f263d47d555a8ee5
leandroleont <leandroleon799@gmail.com>
2025-03-25 15:51:52 -0300

    Ejercio 3d)e)f)

--- tp1_1.c
+++ tp1_1.c
@@ -4,5 +4,15 @@ int main() {
 
     printf("Hola mundo!\n");
 
+    int num = 10;
+
+    int *pnum = &num;
+
+    printf("Contenido del puntero: %d\n", *pnum);
+    printf("Direccion de memoria del puntero: %p\n", pnum);
+    printf("Direccion de memoria de la variable num: %p\n", &num);
+    printf("Direccion de memoria del puntero: %p\n", &pnum);
+    printf("Tamaño de memoria usado por la variable num: %p\n", sizeof(num));
+
     return 0;
 }
